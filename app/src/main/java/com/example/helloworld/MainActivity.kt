e7ff7ab62d614ec8aaaacfc1ae1554f26bff4e40
chrilona <christabelalona@gmail>
2022-05-07 13:49:44 +0300

    Added send money activity

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -7,12 +7,18 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnBmi:Button
+    lateinit var btnSendMoney : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnBmi = findViewById(R.id.btnBmi)
         btnBmi.setOnClickListener {
             val intent=Intent(this,BmiCalculateActivity::class.java)
+            startActivity(intent)
+        }
+        btnSendMoney = findViewById(R.id.btnSendMoney)
+        btnSendMoney.setOnClickListener {
+            val intent = Intent(this, SendM::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/helloworld/SendM.kt
+++ app/src/main/java/com/example/helloworld/SendM.kt
@@ -3,11 +3,9 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class BmiCalculateActivity : AppCompatActivity() {
+class SendM : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_send_m)
     }
-
 }
